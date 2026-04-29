5a289315816d963f969581a71b587c382d8f9468
Sergey Starodub <starodub.sergey0785@gmail.com>
2026-04-29 13:05:27 +0500

    added a file burger.js

---
+++ script/burger.js
@@ -0,0 +1,24 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME_2 = 'body_fixed';    /* добавили 2 к названию - костыль */
+
+const bodyNode2 = document.querySelector('body');   /* добавили 2 к названию - костыль */
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
